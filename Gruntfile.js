7930591fa3a07ef3f5333e5d48ad837741ce8a44
thinkingmedia <mathew@thinkingmedia.ca>
2015-11-28 12:28:36 -0500

    updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,8 @@
  * @param {IGrunt} grunt
  */
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-thinkingmedia');
+    //grunt.loadNpmTasks('grunt-thinkingmedia');
+    grunt.loadTasks('.extra/grunt-thinkingmedia/tasks');
 
     grunt.initConfig({
         config: {
