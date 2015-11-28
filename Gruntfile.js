a0e2ce6c3be1ae85f3e148679cff925b8443a984
thinkingmedia <mathew@thinkingmedia.ca>
2015-11-28 13:05:14 -0500

    builds CSS files now

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,36 @@
  * @param {IGrunt} grunt
  */
 module.exports = function (grunt) {
-    //grunt.loadNpmTasks('grunt-thinkingmedia');
-    grunt.loadTasks('.extra/grunt-thinkingmedia/tasks');
+
+    if(grunt.file.exists('.extra/grunt-thinkingmedia')) {
+        grunt.loadTasks('.extra/grunt-thinkingmedia/tasks');
+    } else {
+        grunt.loadNpmTasks('grunt-thinkingmedia');
+    }
 
     grunt.initConfig({
         config: {
             build: './dist'
+        },
+
+        package: {
+            cssMin: {
+                options: {
+                    clear: [
+                        './dist/css/UI/thinkingmedia-ui.css'
+                    ]
+                },
+                src: './dist/css/UI/thinkingmedia-ui.css',
+                dest: './dist/thinkingmedia-ui.min.css'
+            },
+            cssFull: {
+                src: './www/css/UI/thinkingmedia-ui.css',
+                dest: './dist/thinkingmedia-ui.css'
+            },
+            cssMap: {
+                src: './www/css/UI/thinkingmedia-ui.map',
+                dest: './dist/thinkingmedia-ui.map'
+            }
         }
     });
 };
