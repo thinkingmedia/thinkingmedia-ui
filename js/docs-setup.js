b81e129fd73a1644ff2f53f9b003c44442340653
thinkingmedia <mathew@thinkingmedia.ca>
2016-02-26 09:14:29 -0500

    updated docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -1,378 +1,371 @@
 NG_DOCS={
   "sections": {
-    "api": "API Documentation"
+    "docs": "Documentation"
   },
   "pages": [
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui",
       "shortName": "ui",
       "type": "overview",
       "moduleName": "ui",
       "shortDescription": "ThinkingMedia-UI",
-      "keywords": "add angular api app compiled components controls css dependency factories file href html include js layouts load loaded min module myapp ng-app overview rel script services src styles stylesheet svg text thinkingmedia thinkingmedia-ui type ui var"
+      "keywords": "add angular app compiled components controls css dependency docs factories file href html include js layouts load loaded min module myapp ng-app overview rel script services src styles stylesheet svg text thinkingmedia thinkingmedia-ui type ui var"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components",
       "shortName": "ui.components",
       "type": "overview",
       "moduleName": "ui.components",
       "shortDescription": "UI-Components",
-      "keywords": "api component components directive directives overview perform simple single small task template ui ui-components"
+      "keywords": "component components directive directives docs overview perform simple single small task template ui ui-components"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components.uiDelayHover",
       "shortName": "ui.components.uiDelayHover",
       "type": "directive",
       "moduleName": "ui.components",
       "shortDescription": "Executes the expression after the mouse hovers over the element for 500ms or the value of ui-delay-time attribute.",
-      "keywords": "add api attribute components default delay directive element execute executes executing expression hover hovers long mouse provider set time timeout ui ui-delay-time uidelayhover uidelaytime"
+      "keywords": "add attribute components default delay directive docs element execute executes executing expression hover hovers long mouse provider set time timeout ui ui-delay-time uidelayhover uidelaytime"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components.uiFocus",
       "shortName": "ui.components.uiFocus",
       "type": "directive",
       "moduleName": "ui.components",
       "shortDescription": "Calls el.focus() in the next digest cycle to set the user focus.",
-      "keywords": "api calls components cycle digest directive el focus input set ui uifocus user"
+      "keywords": "calls components cycle digest directive docs el focus input set ui uifocus user"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components.uiReady",
       "shortName": "ui.components.uiReady",
       "type": "directive",
       "moduleName": "ui.components",
       "shortDescription": "Executes the expression after the template has been compiled and digested.",
-      "keywords": "api compiled components digested directive execute executes expression template ui uiready"
+      "keywords": "compiled components digested directive docs execute executes expression template ui uiready"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components.uiRepeat",
       "shortName": "ui.components.uiRepeat",
       "type": "directive",
       "moduleName": "ui.components",
       "shortDescription": "Place this attribute directive on any element that has an ng-repeat on it, and it will add CSS classes",
-      "keywords": "add api assigning attribute change class classes components css default directive element example html item items names ng-repeat place prefix prefixed repeat set string true ui ui-repeat uirepeat uirepeat-directive"
+      "keywords": "add assigning attribute change class classes components css default directive docs element example html item items module names ng-repeat place prefix prefixed repeat set string true ui ui-repeat uirepeat"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.components.uiSize",
       "shortName": "ui.components.uiSize",
       "type": "directive",
       "moduleName": "ui.components",
       "shortDescription": "Executes the expression when the size of an element has been changed.",
-      "keywords": "api assigned change changed changes components current detected directive element execute executed executes expression height hold inner outer service size ui uilayout uisize variables watch width works"
+      "keywords": "assigned change changed changes components current detected directive docs element execute executed executes expression height hold inner outer service size ui uilayout uisize variables watch width works"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls",
       "shortName": "ui.controls",
       "type": "overview",
       "moduleName": "ui.controls",
       "shortDescription": "UI-Controls",
-      "keywords": "api buttons cards compile controls directives forms html inner overview template things ui ui-controls"
+      "keywords": "buttons cards compile controls directives docs forms html inner overview template things ui ui-controls"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiButton",
       "shortName": "ui.controls.uiButton",
       "type": "directive",
       "moduleName": "ui.controls",
       "shortDescription": "Handles the rendering of a UI button.",
-      "keywords": "api button buttons child controls directive handles inherit inheritance inherited rendering services setting size sizes toolbars ui uibutton uisize uitoolbar"
+      "keywords": "button buttons child controls directive docs handles inherit inheritance inherited rendering services setting size sizes toolbars ui uibutton uisize uitoolbar"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiDebug",
       "shortName": "ui.controls.uiDebug",
       "type": "directive",
       "moduleName": "ui.controls",
       "shortDescription": "A developer tool that only displays debug information when the uiDebugService is set to display the info.",
-      "keywords": "api controls debug developer directive display displays info services set tool ui uidebug uidebugservice"
+      "keywords": "controls debug developer directive display displays docs info services set tool ui uidebug uidebugservice"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiDivider",
       "shortName": "ui.controls.uiDivider",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uidivider"
+      "keywords": "controls directive docs ui uidivider"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiIcon",
       "shortName": "ui.controls.uiIcon",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uiicon"
+      "keywords": "controls directive docs ui uiicon"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiMenu",
       "shortName": "ui.controls.uiMenu",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uimenu"
+      "keywords": "controls directive docs ui uimenu"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiMenuItem",
       "shortName": "ui.controls.uiMenuItem",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uimenuitem"
+      "keywords": "controls directive docs ui uimenuitem"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiMenuItems",
       "shortName": "ui.controls.uiMenuItems",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uimenuitems"
+      "keywords": "controls directive docs ui uimenuitems"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiToolbar",
       "shortName": "ui.controls.uiToolbar",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uitoolbar"
+      "keywords": "controls directive docs ui uitoolbar"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiToolbarInput",
       "shortName": "ui.controls.uiToolbarInput",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uitoolbarinput"
+      "keywords": "controls directive docs ui uitoolbarinput"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.controls.uiToolTip",
       "shortName": "ui.controls.uiToolTip",
       "type": "directive",
       "moduleName": "ui.controls",
-      "keywords": "api controls directive ui uitooltip"
+      "keywords": "controls directive docs ui uitooltip"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.factories",
       "shortName": "ui.factories",
       "type": "overview",
       "moduleName": "ui.factories",
       "shortDescription": "UI-Factories",
-      "keywords": "allow angular api classes factories factory function inject overview ui ui-factories"
+      "keywords": "allow angular classes docs factories factory function inject overview ui ui-factories"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.factories.$modalContextFactory",
       "shortName": "ui.factories.$modalContextFactory",
       "type": "service",
       "moduleName": "ui.factories",
-      "keywords": "$modalcontextfactory api factories service ui"
+      "keywords": "$modalcontextfactory docs factories service ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts",
       "shortName": "ui.layouts",
       "type": "overview",
       "moduleName": "ui.layouts",
-      "keywords": "api layouts overview ui"
+      "keywords": "docs layouts overview ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiBorder",
       "shortName": "ui.layouts.uiBorder",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uiborder"
+      "keywords": "directive docs layouts ui uiborder"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiBottom",
       "shortName": "ui.layouts.uiBottom",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uibottom"
+      "keywords": "directive docs layouts ui uibottom"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiCenter",
       "shortName": "ui.layouts.uiCenter",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uicenter"
+      "keywords": "directive docs layouts ui uicenter"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiColumns",
       "shortName": "ui.layouts.uiColumns",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uicolumns"
+      "keywords": "directive docs layouts ui uicolumns"
     },
     {
-      "section": "api",
-      "id": "ui.layouts.uiLayout",
-      "shortName": "ui.layouts.uiLayout",
-      "type": "directive",
-      "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uilayout"
-    },
-    {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiLeft",
       "shortName": "ui.layouts.uiLeft",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uileft"
+      "keywords": "directive docs layouts ui uileft"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiRight",
       "shortName": "ui.layouts.uiRight",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uiright"
+      "keywords": "directive docs layouts ui uiright"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.layouts.uiTop",
       "shortName": "ui.layouts.uiTop",
       "type": "directive",
       "moduleName": "ui.layouts",
-      "keywords": "api directive layouts ui uitop"
+      "keywords": "directive docs layouts ui uitop"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services",
       "shortName": "ui.services",
       "type": "overview",
       "moduleName": "ui.services",
-      "keywords": "api overview services ui"
+      "keywords": "docs overview services ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiDebugService",
       "shortName": "ui.services.uiDebugService",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uidebugservice"
+      "keywords": "docs service services ui uidebugservice"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiEvents",
       "shortName": "ui.services.uiEvents",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uievents"
+      "keywords": "docs service services ui uievents"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiLayout",
       "shortName": "ui.services.uiLayout",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uilayout"
+      "keywords": "docs service services ui uilayout"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiModal",
       "shortName": "ui.services.uiModal",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uimodal"
+      "keywords": "docs service services ui uimodal"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiSize",
       "shortName": "ui.services.uiSize",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uisize"
+      "keywords": "docs service services ui uisize"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.services.uiTimeout",
       "shortName": "ui.services.uiTimeout",
       "type": "service",
       "moduleName": "ui.services",
-      "keywords": "api service services ui uitimeout"
+      "keywords": "docs service services ui uitimeout"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg",
       "shortName": "ui.svg",
       "type": "overview",
       "moduleName": "ui.svg",
-      "keywords": "api overview svg ui"
+      "keywords": "docs overview svg ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.$svgPath",
       "shortName": "ui.svg.$svgPath",
       "type": "service",
       "moduleName": "ui.svg",
-      "keywords": "$svgpath api service svg ui"
+      "keywords": "$svgpath docs service svg ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.$svgTurtle",
       "shortName": "ui.svg.$svgTurtle",
       "type": "service",
       "moduleName": "ui.svg",
-      "keywords": "$svgturtle api service svg ui"
+      "keywords": "$svgturtle docs service svg ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.d3",
       "shortName": "ui.svg.d3",
       "type": "service",
       "moduleName": "ui.svg",
-      "keywords": "api d3 service svg ui"
+      "keywords": "d3 docs service svg ui"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.uiGrid",
       "shortName": "ui.svg.uiGrid",
       "type": "directive",
       "moduleName": "ui.svg",
-      "keywords": "api directive svg ui uigrid"
+      "keywords": "directive docs svg ui uigrid"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.uiSvgDrag",
       "shortName": "ui.svg.uiSvgDrag",
       "type": "directive",
       "moduleName": "ui.svg",
-      "keywords": "api directive svg ui uisvgdrag"
+      "keywords": "directive docs svg ui uisvgdrag"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.uiSvgGrip",
       "shortName": "ui.svg.uiSvgGrip",
       "type": "directive",
       "moduleName": "ui.svg",
-      "keywords": "api directive svg ui uisvggrip"
+      "keywords": "directive docs svg ui uisvggrip"
     },
     {
-      "section": "api",
+      "section": "docs",
       "id": "ui.svg.uiSvgZoom",
       "shortName": "ui.svg.uiSvgZoom",
       "type": "directive",
       "moduleName": "ui.svg",
-      "keywords": "api d3 directive svg ui uisvgzoom"
+      "shortDescription": "Applies the d3 Zoom behavior to an element by updating a transform.",
+      "keywords": "applies behavior d3 directive docs element svg transform ui uisvgzoom updating zoom"
     }
   ],
   "apis": {
-    "api": true
+    "docs": true
   },
   "html5Mode": false,
   "editExample": true,
-  "startPage": "/api",
+  "startPage": "/docs",
   "scripts": [
     "angular.min.js"
   ]
